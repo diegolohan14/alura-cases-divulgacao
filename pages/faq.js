1909236d8895d3adb50c21729829998611004541
diegolohan14 <diegolohan14@gmail.com>
2022-04-08 08:55:25 -0300

    Enter

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from '../scr/components/Link';
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Alura Cases - Páginas de perguntas FAQ</h1>
+            <Link  href="/" passHref>
+                Ir para a home
+            </Link>
+        </div>
+    )
+}
