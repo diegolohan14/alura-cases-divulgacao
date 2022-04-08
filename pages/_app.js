18a99abcd757a8faa899d2d1138e87bcda435677
diegolohan14 <diegolohan14@gmail.com>
2022-04-08 09:17:47 -0300

    Atualizações nos estilos e nas fontes

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import GlobalStyle from "../scr/theme/GlobalStyle";
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    );
+}
+
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import Link from '../scr/components/Link';
 
-function GlobalStyle(){
-    return(
-        <style global jsx>{`
-            body {
-                font-family: sans-serif;
-            }
-        `}</style>
-    );
-}
 
 function Title({ children, as }){ 
     const Tag = as;
@@ -30,8 +21,7 @@ function Title({ children, as }){
 export default function HomePage() {
     return (
         <div>
-            <GlobalStyle />
-            <Title as="h2">Alura Cases - Home Page</Title>
+            <Title as="h1">Alura Cases - Home Page</Title>
             <Link href="/faq">
                 Ir para a página do FAQ
             </Link>
